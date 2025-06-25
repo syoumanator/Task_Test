f0258b435ba0bf6ce97dee4b6626437f075b947c
Alik <alik220201@gmail.com>
2025-06-25 21:31:14 +0700

    add urls fixes

--- config/settings.py
+++ config/settings.py
@@ -86,8 +86,9 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
